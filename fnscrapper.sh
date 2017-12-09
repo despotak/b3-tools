d6cd6a14d4727e7163cfe4f1e44b42988824b71e
@archangel <christos@despotak.is>
2017-12-09 17:47:58 +0200

    Added script header

--- fnscrapper.sh
+++ fnscrapper.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+## Usage: ./fnscrapper.sh <starting_block> <ending_block> (if you just want to see the results in the console
+##
+## Prints Fundamental Node winners for each block, along with the respective reward
+##
+## Author(s):
+##	cdr Archangel
+##
+## version: 0.0.1
+##
 for i in `seq $1 $2`;
 do
 	TXID=$(~/b3/github/B3-CoinV2/src/b3coind getblockbynumber $i | jq .tx[1])
