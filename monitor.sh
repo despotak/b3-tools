d2939e245a5557112ea01d3e2bccb8aa92a5166b
Christos Despotakis <despotak@users.noreply.github.com>
2017-12-18 13:23:08 +0200

    Change hostname to display the FQDN

--- monitor.sh
+++ monitor.sh
@@ -52,7 +52,7 @@ last_block=
 ##MAIN LOGIC##
 
 #Machine info
-host=$(hostname)
+host=$(hostname -f)
 ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
 #Node info
