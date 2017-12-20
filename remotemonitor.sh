fab14e1e9c3ee9dc9bea94f89beac611155b73bb
Christos Cdr Archangel Despotakis <christos@despotak.is>
2017-12-20 10:38:12 +0200

    Remove old stuff

--- remotemonitor.sh
+++ remotemonitor.sh
@@ -34,23 +34,3 @@ for i in $(seq -f "%03g" 1 2); do
 	echo -e host: $host'\t'connections: $(printf "%03g" $connectioncount)'\t'block: $blockcount "(" $blockhash ")"'\t'Uptime \& load: $uptime'\t' Mem: $memory "MB" Swap: $swap "MB"
 
 done
-
-
-#chainz_block=$(curl -s https://chainz.cryptoid.info/b3/api.dws?q=getblockcount)
-#url="https://chainz.cryptoid.info/b3/api.dws?q=getblockhash;height="$chainz_block
-#chainz_hash=$(trim $(curl -s $url))
-
-#echo -e host: chainz.cryptoid.info '\t'connections: "???"'\t'block: $chainz_block "(" $chainz_hash ")"
-
-#host=$seed0001
-#blockcount=$(ssh $seed0001 /home/fn/b3coind getblockcount)
-#connectioncount=$(ssh $seed0001 /home/fn/b3coind getconnectioncount)
-#blockhash=$(ssh $seed0001 /home/fn/b3coind getblockhash $blockcount)
-#echo -e host: $host'\t'connections: $connectioncount'\t'block: $blockcount "(" $blockhash ")"
-
-
-#host=$seed0002
-#blockcount=$(ssh $seed0002 /home/fn/b3coind getblockcount)
-#connectioncount=$(ssh $seed0002 /home/fn/b3coind getconnectioncount)
-#blockhash=$(ssh $seed0002 /home/fn/b3coind getblockhash $blockcount)
-#echo -e host: $host'\t'connections: $connectioncount'\t'block: $blockcount "(" $blockhash ")"
